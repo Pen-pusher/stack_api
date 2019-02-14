41359129e1b37b1bde15d08d8be7e12420c1e378
ravi11o <ravi.suraj110@gmail.com>
2019-02-14 17:19:20 +0530

    add questions model

---
+++ models/Question.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var questionSchema = new Schema({
+  title: { type: String, required: true, index: true },
+  description: { type: String, required: true },
+  upvote: { type: Number, default: 0 },
+  stars: { type: Number, default: 0 },
+  viewCount: { type: Number, default: 0 },
+  authorId: { type: Schema.Types.ObjectId, required: true, ref: 'User' }
+}, {timestamps: true});
+
+module.exports = mongoose.model('Question', questionSchema);
