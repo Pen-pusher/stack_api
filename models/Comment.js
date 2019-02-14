9fcd5947603810cfee932b9679e52fa2c0d24105
ravi11o <ravi.suraj110@gmail.com>
2019-02-14 17:27:16 +0530

    add comment schema

---
+++ models/Comment.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var commentSchema = new Schema({
+  description: { type: String, required: true },
+  questionId: { type: Schema.Types.ObjectId, ref: 'Question' },
+  answerId: { type: Schema.Types.ObjectId, ref: 'Answer' },
+  authorId: { type: Schema.Types.ObjectId, required: true, ref: 'User'},
+  upvote: { type: Number, default: 0 }
+}, { timestamps: true } );
